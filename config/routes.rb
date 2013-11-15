6a3fd11eebfc0a227e678918d310893b6ecd7cc5
Mathieu <macomeau@contactnorth.ca>
2013-11-15 10:40:29 -0500

    end of chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
 	resources :users
-  
+	resource :sessions, only: [:new, :create, :destroy]
 	root 'statc_pages#home'
 	  match '/signup', to: 'users#new', via: 'get'
+	  match '/signin', to: 'sessions#new', via: 'get'
+	  match '/signout', to: 'sessions#destroy', via: 'delete'
 	  match '/help', to: 'statc_pages#help', via: 'get'
 	  match '/about', to: 'statc_pages#about', via: 'get'
 	  match '/contact', to: 'statc_pages#contact', via: 'get'
